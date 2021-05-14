04d04c2a70adc126090ff1ba3ca86d9cbd190e64
lumen <gutterfly@gmail.com>
2021-05-14 12:41:17 -0700

    add hashing. remove env var.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
-require('dotenv').config();
+
+const md5 = require("md5");
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -27,12 +27,6 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-
-////////mongoose-encryption (using one key)
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
-
-
 const User = new mongoose.model("User", userSchema);
 
 ///HOME////
@@ -50,7 +44,7 @@ app.route("/register")
   .post(function(req, res) {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
     newUser.save(function(err) {
       if (!err) {
@@ -69,7 +63,7 @@ app.route("/login")
   })
   .post(function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({
       email: username
